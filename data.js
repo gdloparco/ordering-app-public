9231bc81edd215e5c135882422a8b6b47c2cae19
Dom <125898079+gdloparco@users.noreply.github.com>
2024-03-25 21:39:41 +0000

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const menuArray = [
         ingredients: ["Tomato Sauce", " Mozzarella", " Basil"],
         id: 0,
         price: 12.00,
-        image: "./pizza.png",
+        image: "/pizza.png",
         property: "pizza",
     },
     {
@@ -12,7 +12,7 @@ export const menuArray = [
         ingredients: ["Cone", " Salted-caramel", " Vanilla"],
         id: 1,
         price: 5.00,
-        image: "./ice_cream.png",
+        image: "/ice_cream.png",
         property: "ice-cream",
     },
         {
@@ -20,7 +20,7 @@ export const menuArray = [
         ingredients: ["Grain", " Hops" , " Yeast", " Water"],
         id: 2,
         price: 6.00,
-        image: "./beer.png",
+        image: "/beer.png",
         property: "beer",
     }
 ]
